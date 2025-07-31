862d95cd1e2199cb5e384ea48ffabaa61fda776a
Chaman <ckagrawal89@gmail.com>
2025-08-01 01:52:31 +0530

    Updated Insight tools

--- src/qloo_mcp_server/get_insights.py
+++ src/qloo_mcp_server/get_insights.py
@@ -4,37 +4,8 @@ from typing import Optional, Dict, Any
 from urllib.parse import quote
 from src.qloo_mcp_server.constant import QLOO_API, QLOO_API_KEY
 import json
+from src.qloo_mcp_server.utils import encode_form_query
 
-
-def encode_form_query(data: Dict[str, Any], explode: bool = False) -> str:
-    def encode_value(value):
-        if isinstance(value, list):
-            return ",".join(quote(str(v), safe="")) if not explode else [
-                (key, quote(str(v), safe="")) for v in value
-            ]
-        elif value is None:
-            return None
-        return quote(str(value), safe="")
-
-    query_params = []
-    print(f"Encoding query parameters: {data}")
-    for key, value in data.items():
-        if value is None:
-            continue
-        encoded_key = quote(key, safe="")
-
-        if isinstance(value, list):
-            if explode:
-                for v in value:
-                    query_params.append(f"{encoded_key}={quote(str(v), safe='')}")
-            else:
-                joined = ",".join(quote(str(v), safe="") for v in value)
-                query_params.append(f"{encoded_key}={joined}")
-        else:
-            encoded_value = quote(str(value), safe="")
-            query_params.append(f"{encoded_key}={encoded_value}")
-
-    return "&".join(query_params)
 
 def clean_response(response: httpx.Response) -> Dict[str, Any]:
     keys_to_remove = [
